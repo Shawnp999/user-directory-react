b6161ad550e00cff0e62d52018e62bf276f3129f
Shawn Pantzlaff <2001shawnp@gmail.com>
2024-07-25 10:07:11 +0500

    checkpoint

--- src/webparts/uzmtoUserDirectory/components/usercard/UserCard.tsx
+++ src/webparts/uzmtoUserDirectory/components/usercard/UserCard.tsx
@@ -1,25 +1,35 @@
+// UserCard.tsx
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import styles from './usercardcss.module.scss';
 import { IAddtionalUserSettings } from '../entities/AdditionalUserSettings';
 import * as strings from 'UzmtoUserDirectoryWebPartStrings';
 import { IDepartment } from '../departments/entity/Departmen';
-//import { Department } from '../departments/entity/Departmen';
 
 interface IUserCardProps {
   user: MicrosoftGraph.User;
   manager: MicrosoftGraph.User;
   additionalUserSettings: IAddtionalUserSettings | undefined;
   highlightEmail: (emailFromAD: string | undefined, userId?: string) => string;
-  myDepartment : IDepartment | null | undefined
-  // loggedInUser?: MicrosoftGraph.User;
-  
+  myDepartment: IDepartment | null | undefined;
 }
 
-const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSettings, myDepartment}) => {
+const UserCard: React.FC<IUserCardProps> = ({ user, manager, additionalUserSettings, myDepartment }) => {
+  const [isMobileView, setIsMobileView] = useState<boolean>(window.innerWidth < 900);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 900);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const transformText = (text: string, minLength: number, maxParts: number): React.ReactNode => {
-
     if (!text || text.length <= minLength) {
       return text || '';
     }
@@ -36,12 +46,10 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
         <br />
         {textParts.slice(maxParts).join(' ')}
       </>
-
     );
   };
 
   const getManagerNameLetters = (displayName: string): string => {
-
     if (!displayName) {
       return '';
     }
@@ -59,12 +67,10 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
   };
 
   const formatMobilePhone = (mobilePhone: string | undefined): string => {
-
     if (!mobilePhone) {
       return '';
     }
 
-    //check if phonenumber has +
     if (mobilePhone.startsWith('+')) {
       return formatPhoneNumber(mobilePhone);
     } else {
@@ -73,13 +79,9 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
   };
 
   const formatPhoneNumber = (phoneNumber: string): string => {
-
     const cleanedPhoneNumber = phoneNumber.replace(/\D/g, '');
-
-    //format phone number to +xxx xx xxx xx xx format
     return cleanedPhoneNumber.replace(/(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})/, '+$1 $2 $3 $4 $5');
   };
-  //console.log("media insta",media.instagram)
 
   const transformedDisplayName = transformText(user.displayName!, 40, 2);
   const transformedDepartmentName = transformText(user.department || '', 50, 2);
@@ -90,10 +92,8 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
   const userPhotoUrl = 'https://eneraseg.sharepoint.com/sites/UZMTO2/foto_employees/' + username + '/big_pic.jpg';
 
   const isSpecialUser = user.id === '6248d93e-b3dd-4fdf-9d94-2c7a12f0a6dc';
-
   const isManagerUndefined = userManager === null || userManager === undefined;
 
-  //socials links
   const instLink = additionalUserSettings?.media.instagram;
   const instaURL = instLink ? `https://www.instagram.com/${instLink}` : '';
 
@@ -112,10 +112,9 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
   const NoUserPhotoUrl = `https://eneraseg.sharepoint.com/sites/UZMTO2/foto_employees/NoPhoto/nophoto.jpg`;
 
   const highlightEmail = (emailFromAD: string | undefined): JSX.Element => {
-
     const isUzmtoEmail = emailFromAD?.toLowerCase().endsWith('@uzmto.com');
     const isITDepartmentUser = myDepartment?.departmentName === 'Департамент информационных технологий';
-  
+
     return (
       <span style={{ color: isUzmtoEmail || !isITDepartmentUser ? 'inherit' : 'red' }}>
         {emailFromAD}
@@ -124,10 +123,9 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
   };
 
   return (
-    <div className={styles.cell}>
-      <div className={styles.userContainer}>
-        <div className={styles.photoContainer}>
-
+    <div className={isMobileView ? styles.cellMobile : styles.cell}>
+      <div className={isMobileView ? styles.userContainerMobile : styles.userContainer}>
+        <div className={isMobileView ? styles.photoContainerMobile : styles.photoContainer}>
           <img
             src={userPhotoUrl || NoUserPhotoUrl}
             alt="User Photo"
@@ -140,25 +138,24 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
             }}
             style={{ width: '100%', cursor: 'pointer', height: '100%', objectFit: 'cover', borderRadius: '10px' }}
           />
-
         </div>
-        <div className={styles.userInfoContainer}>
+        <div className={isMobileView ? styles.userInfoContainerMobile : styles.userInfoContainer}>
           <div
-            className={styles.displayName + ' ' + styles.userInfoCnt}
-            // onClick={() => (window.location.href = `https://eneraseg.sharepoint.com/sites/UZMTO2/SitePages/profile-page.aspx?userId=${user.id}`)}
-            //style={{ cursor: 'pointer' }}
+            className={isMobileView ? styles.displayNameMobile + ' ' + styles.userInfoCntMobile  : styles.displayName + ' ' + styles.userInfoCnt}
           >
+            <div className={styles.displayNameLargeMobile} >
             {transformedDisplayName}
-            <br />
-            <div className={styles.jobTitle} style={{ color: '#b1b2c4', fontFamily: 'medium' }}>
-              {user.jobTitle} 
+            </div>
+            
+            <div className={isMobileView ? styles.jobTitleMobile : styles.jobTitle} style={{ color: '#b1b2c4', fontFamily: 'medium' }}>
+              {user.jobTitle}
             </div>
           </div>
-          <div className={`${styles.department} ${styles.userInfoCnt}`}>
+          <div className={isMobileView ? styles.departmentMobile + ' ' + styles.userInfoCntMobile : styles.department + ' ' + styles.userInfoCnt}>
             <div>{transformedDepartmentName}</div>
             {!isSpecialUser && (
               <div
-                className={styles.managerLetters}
+                className={isMobileView ? styles.managerLettersMobile : styles.managerLetters}
                 onClick={() => (window.location.href = `https://eneraseg.sharepoint.com/sites/UZMTO2/SitePages/profile-page.aspx?userId=${manager.id}`)}
                 style={{
                   cursor: 'pointer',
@@ -171,16 +168,16 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
               </div>
             )}
           </div>
-          <div className={styles.department + ' ' + styles.userInfoCnt}>
+          <div className={isMobileView ? styles.departmentMobile + ' ' + styles.userInfoCntMobile : styles.department + ' ' + styles.userInfoCnt}>
             {highlightEmail(user.mail?.toString())}
           </div>
-          <div className={`${styles.department} ${styles.userInfoCnt}`} style={{ justifyContent: 'unset' }}>
+          <div className={isMobileView ? styles.departmentMobile + ' ' + styles.userInfoCntMobile: styles.department + ' ' + styles.userInfoCnt} style={{ justifyContent: 'unset' }}>
             <span className={styles.spanclass}>{strings.UserInfo_InnerPhone}: &nbsp;</span> {user.businessPhones?.join(',')}
           </div>
-          <div className={`${styles.department} ${styles.userInfoCnt}`}>{formatMobilePhone(user.mobilePhone!)}</div>
-          <div className={styles.socialIconContainer}>
-            <div className={styles.socialIconsLeft}></div>
-            <div className={styles.socialIconsRight}>
+          <div className={isMobileView ? styles.departmentMobile + ' ' + styles.userInfoCntMobile : styles.department + ' ' + styles.userInfoCnt}>{formatMobilePhone(user.mobilePhone!)}</div>
+          <div className={isMobileView ? styles.socialIconContainerMobile : styles.socialIconContainer}>
+            <div className={isMobileView ? styles.socialIconsLeftMobile : styles.socialIconsLeft}></div>
+            <div className={isMobileView ? styles.socialIconsRightMobile : styles.socialIconsRight}>
               {additionalUserSettings && additionalUserSettings.media.meta && <a href={MetaURL} target="_blank" rel="noopener noreferrer" className={`${styles.socialIcon} ${styles.meta}`} />}
               {additionalUserSettings && additionalUserSettings.media.instagram && (
                 <div className={styles.instasocials}>
@@ -203,8 +200,5 @@ const UserCard: React.FC<IUserCardProps>  = ({ user, manager, additionalUserSett
     </div>
   );
 }
-
-
-
 
 export default UserCard;
